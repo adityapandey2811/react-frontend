27532f058f6d303a8da6cc616194503029d269bb
rakshitKaushik <kartikkaushik544@gmail.com>
2023-11-15 04:07:57 -0800

    Added Proper Functionality for Login

--- online-insurance-app/src/App.js
+++ online-insurance-app/src/App.js
@@ -1,24 +1,14 @@
-import React from 'react';
-import { Provider } from 'react-redux';
-import store from './redux/store';
-import LoginPage from './LoginPage';
+import React from "react";
+import LoginPage from "./LoginPage";
+import AdminPage from "./components/AdminPage";
 
 function App() {
   return (
-    <Provider store={store}>
-      <div className="App">
-        <LoginPage />
-      </div>
-    </Provider>
+    <div className="App">
+      <LoginPage />
+      {/* <AdminPage /> */}
+    </div>
   );
-// import logo from "./logo.svg";
-// import "./App.css";
-// import AdminPage from "./components/AdminPage";
-
-// function App() {
-//   return <div className="App">
-//     <AdminPage />
-//   </div>;
 }
 
 export default App;

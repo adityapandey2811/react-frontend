0074b02b29146a3df768941013d0b95cf69e9df2
rakshitKaushik <kartikkaushik544@gmail.com>
2023-11-16 23:12:49 -0800

    Admin

--- online-insurance-app/src/pages/Routers.jsx
+++ online-insurance-app/src/pages/Routers.jsx
@@ -8,11 +8,13 @@ import AdminPage from "./AdminPage";
 import PoliciesPage from "./AllPoliciesPage/PoliciesPage";
 import AboutUs from "./AboutUs/AboutUs";
 import ForgotPasswordPage from "./ForgetPasswordPage/ForgetPasswordPage";
-import FeedbackFormPage from "./FeedbackPage/FeedbackForm"
-import FeedbackListPage from "./FeedbackPage/FeedbackList"
+import FeedbackFormPage from "./FeedbackPage/FeedbackForm";
+import FeedbackListPage from "./FeedbackPage/FeedbackList";
 import OrdersPage from "./OrdersPage/OrdersPage";
+import { useSelector } from "react-redux";
 
 function Routers(props) {
+  const userName = useSelector((state) => state.auth.user);
   return (
     <BrowserRouter>
       <Routes>
@@ -21,12 +23,13 @@ function Routers(props) {
         <Route path="/cart" element={<CartPage />} />
         <Route path="/login" element={<LoginPage />} />
         <Route path="/register" element={<RegistrationPage />} />
-        <Route path="/admin" element={<AdminPage />} />
+        {userName == "admin" && <Route path="/admin" element={<AdminPage />} />}
         <Route path="/about" element={<AboutUs />} />
         <Route path="/forgetPass" element={<ForgotPasswordPage />} />
         <Route path="/feedback/:id" element={<FeedbackFormPage />} />
         <Route path="/feedbackList" element={<FeedbackListPage />} />
         <Route path="/orders" element={<OrdersPage />} />
+        <Route path="/*" element={<HomePage />} />
       </Routes>
     </BrowserRouter>
   );
